c10e3eee5cb70f60a68f3ce2bb6c49bc2b193d5b
Paulo <paulohsantosrocha@gmail.com>
2023-11-29 11:34:20 -0300

    ✨feat: convidar novos amigos

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -18,6 +18,7 @@ import {
   User,
   UserPlus,
 } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -25,6 +26,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -38,7 +41,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuCheckboxItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuCheckboxItem
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen("invite", { server })}
+          >
             Invite people
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuCheckboxItem>
